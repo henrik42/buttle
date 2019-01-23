dd7e036865c20337e3304c668d27ab855e40bcb0
U-INT\heineh <heineh@msgn08198.int.root.msg.ag>
2019-01-23 10:55:16 +0100

    jaeger tracing

--- examples/buttle/examples/open_tracing.clj
+++ examples/buttle/examples/open_tracing.clj
@@ -25,29 +25,63 @@
           (.withReporter logging-remote-reporter))
         .build)))
 
+;; at the moment we're creating/using just one global Jaeger tracer
 (def jaeger-tracer
   (build-jaeger-tracer
    (System/getProperty "buttle_jaeger_agent_host")
-   "buttle-connection-trace"))
+   "buttle-trace-2"))
+
+;; this will be presented as "Operation" in the Jaeger GUI
+(defn method->operation [m]
+  (format "%s/%s"
+          (-> (.getDeclaringClass m)
+              .getName)
+          (.getName m)))
+
+;; tags for all spans
+(defn ->tags [the-method target-obj the-args]
+  {:method-name (.getName the-method)
+   :method (format "%s/%s"
+                   (-> (.getDeclaringClass the-method)
+                       .getName)
+                   (.getName the-method))
+   :class-name (-> (.getDeclaringClass the-method)
+                   .getSimpleName)
+   :class (-> (.getDeclaringClass the-method)
+                   .getName)
+   :args (into [] the-args)})
+
+(defn throwable->tags [t]
+  {:type 'throw
+   :throw t})
+
+(defn result->tags [r]
+  {:type 'return
+   :return r})
 
 (defn invoke-with-tracing [the-method target-obj the-args]
   (binding [tracing/*tracer* jaeger-tracer]
-    (tracing/with-span [s {:name (format "method:%s" the-method)}]
+    (tracing/with-span [s {:name (method->operation the-method)}]
+      (tracing/set-tags (->tags the-method target-obj the-args))
       (let [r (try
                 (proxy/handle-default the-method target-obj the-args)
                 (catch Throwable t
                   (do
+                    (tracing/set-tags (throwable->tags t))
                     (throw t))))]
+        (tracing/set-tags (result->tags r))
         r))))
 
-;; trace all/any class/method
+#_ ;; trace all/any class/method
 (defmethod proxy/handle :default [the-method target-obj the-args]
   (invoke-with-tracing the-method target-obj the-args))
 
-#_
+(proxy/def-handle [java.sql.Driver :buttle/default] [the-method target-obj the-args]
+  (invoke-with-tracing the-method target-obj the-args))
+
 (proxy/def-handle [java.sql.Connection :buttle/default] [the-method target-obj the-args]
   (invoke-with-tracing the-method target-obj the-args))
 
-#_
-(proxy/def-handle [java.sql.Driver :buttle/default] [the-method target-obj the-args]
+(proxy/def-handle [java.sql.Statement :buttle/default] [the-method target-obj the-args]
   (invoke-with-tracing the-method target-obj the-args))
+
