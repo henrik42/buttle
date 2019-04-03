ed4cf006fc08d9f4e8304f757d6a4362ca5a9182
henrik42 <heineh@msgn08198.int.root.msg.ag>
2019-04-03 18:57:58 +0200

    docu

--- plugin/leiningen/deploy_driver.clj
+++ plugin/leiningen/deploy_driver.clj
@@ -3,6 +3,22 @@
             [leiningen.deploy :as ldeploy]
             [leiningen.core.main :as main]))
 
+;; Original code copied from
+;; https://github.com/technomancy/leiningen/blob/2.8.1/src/leiningen/deploy.clj
+;;
+;; The original calculates the :classifier from the given filename and
+;; the version. See
+;;
+;; https://github.com/technomancy/leiningen/blob/2.8.1/src/leiningen/deploy.clj#L228
+;;
+;; For Buttle we want to build `target/uberjar/buttle-driver.jar`
+;; which does not carry the version number. So here you pass in the
+;; classifier. As an add-on this function supports pseudo repository
+;; :leiningen/repository ("snapshots" for SNAPSHOT version, else
+;; "releases") and pseudo versions :leiningen/version (means: take
+;; version from project). This makes it easy to invoke this task
+;; through :release-tasks.
+
 (defn deploy-driver
   "Deploys the Buttle driver (UBERJAR) to the given repo.
 
