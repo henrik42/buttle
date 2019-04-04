7e2278aaf5c8876ebe8ee8608fa3f77fd2868933
henrik42 <heineh@msgn08198.int.root.msg.ag>
2019-04-04 23:51:27 +0200

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
 (cemerick.pomegranate.aether/register-wagon-factory!
  "http" #(org.apache.maven.wagon.providers.http.HttpWagon.))
 
-(defproject buttle/buttle "0.1.3"
+(defproject buttle/buttle "0.1.4-SNAPSHOT"
 
   :description "Buttle is a proxying JDBC driver with hooks."
   
